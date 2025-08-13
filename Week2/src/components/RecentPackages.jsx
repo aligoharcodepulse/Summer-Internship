21b7ec207ca9ea06f87b782bcc7928a4dec5e53e
Ali Gohar <aligoharkp@gmail.com>
2025-08-13 20:41:23 +0500

    Week2-added recent packages section

--- Week2/src/components/RecentPackages.jsx
+++ Week2/src/components/RecentPackages.jsx
@@ -6,19 +6,19 @@ const packages = [
     country: "Switzerland",
     days: 7,
     price: "$2,499",
-    image: "https://images.unsplash.com/photo-1501785888041-af3ef285b470"
+    image: "./public/images/switzerland.png"
   },
   {
     country: "Japan",
     days: 10,
     price: "$3,299",
-    image: "https://images.unsplash.com/photo-1558981001-6f0c94958bb6"
+    image: "./public/images/japan.png"
   },
   {
     country: "Italy",
     days: 5,
     price: "$1,899",
-    image: "https://images.unsplash.com/photo-1507525428034-b723cf961d3e"
+    image: "./public/images/italy.png"
   }
 ];
 
@@ -26,9 +26,9 @@ export default function RecentPackages() {
   return (
     <Grid container spacing={3} sx={{ mt: 5, px: 3 }}>
       {packages.map((pkg, index) => (
-        <Grid item xs={12} sm={6} md={4} key={index}>
-          <Card sx={{ borderRadius: 3, boxShadow: 4 }}>
-            <CardMedia component="img" height="200" image={pkg.image} alt={pkg.country} />
+        <Grid item xs={12} sm={6} md={4} key={index} alignItems={'center'}>
+          <Card sx={{ borderRadius: 3, boxShadow: 4, ml:1 }}>
+            <CardMedia component="img" height="300" image={pkg.image} alt={pkg.country} />
             <CardContent>
               <Typography variant="h5" fontWeight="bold">
                 {pkg.country}
