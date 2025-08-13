2c29d57fe632ae6852296b8d9e7106f52b08f81f
Ali Gohar <aligoharkp@gmail.com>
2025-08-13 16:22:10 +0500

    carousel added in home page

--- Week2/src/components/Home.jsx
+++ Week2/src/components/Home.jsx
@@ -1,10 +1,9 @@
-
+import Carousel from "./Carousel";
 const Home = () => {
 
     return(
         <>
-        <h1 style={{marginTop:100}}></h1>
-        Home
+        <Carousel />
         </>
     )
 } 
